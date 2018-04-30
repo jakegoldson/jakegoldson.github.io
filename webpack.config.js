acee38f09b7655d851e73c8ba2984929104fe376
Jake Goldson <jake.v.d.g@gmail.com>
2018-05-01 01:37:29 +0200

    added packges, added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  watchOptions: {
+    ignored: /node_modules/
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
