bd170e276a7709df7a1b1db3e9f2321088edc880
Jake Goldson <jake.goldson@ispy.se>
2022-10-22 15:17:12 +0100

    add dist + deploy script

--- README.md
+++
@@ -1,1 +0,0 @@
-# jakegoldson.github.io
--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import './style.css'
-
-const doc = document.documentElement;
-
-document.addEventListener('mousemove', e => {
-  doc.style.setProperty('--mouse-x', (e.clientX / window.innerWidth).toString());
-  doc.style.setProperty('--mouse-y', (e.clientY / window.innerHeight).toString());
-});
